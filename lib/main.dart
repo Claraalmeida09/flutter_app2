96666f0ac8328745ee14a566334a661edfae83c2
Claraalmeida09 <cllara.almeida09@gmail.com>
2020-09-06 19:33:46 -0300

    In Progress - implementação do armazenamento interno

---
+++ lib/main.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytebankAPP());
+}
+
+class BytebankAPP extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColor: Colors.pink[300],
+            accentColor: Colors.pink[700]
+        ),
+        home: ListaTransferencias(),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FormularioTransferenciaState();
+  }
+}
+
+class FormularioTransferenciaState extends State<FormularioTransferencia> {
+  final TextEditingController _controladorCampoNumeroConta =
+  TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // leading: Icon(Icons.monetization_on),
+          title: Text('Criando Transferências'),
+          // backgroundColor: Colors.pinkAccent,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Editor(
+                  controlador: _controladorCampoNumeroConta,
+                  rotulo: 'Numero Conta',
+                  dica: '0000'),
+              Editor(
+                  controlador: _controladorCampoValor,
+                  rotulo: 'Valor da Transferência',
+                  dica: '0.00',
+                  icone: Icons.monetization_on),
+              RaisedButton(
+                color: Colors.pinkAccent,
+                child: Text(
+                  'Confirmar',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => _criaTransferencia(context),
+              )
+            ],
+          ),
+        ));
+  }
+
+  void _criaTransferencia(BuildContext context) {
+    final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      debugPrint('Criando Transferencia');
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
+
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+          controller: controlador,
+          style: TextStyle(fontSize: 24.0),
+          decoration: InputDecoration(
+              icon: icone != null ? Icon(icone) : null,
+              labelText: rotulo,
+              hintText: dica),
+          keyboardType: TextInputType.number),
+    );
+  }
+}
+
+class ListaTransferencias extends StatefulWidget {
+  final List<Transferencia> _transferencias = List();
+
+  @override
+  State<StatefulWidget> createState() {
+    return ListaTransferenciasState();
+  }
+}
+
+class ListaTransferenciasState extends State<ListaTransferencias> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.monetization_on),
+        title: Text('Transferências'),
+      ),
+      body: ListView.builder(
+          itemCount: widget._transferencias.length,
+          itemBuilder: (context, indice) {
+            final transferencia = widget._transferencias[indice];
+            return ItemTransferencia(transferencia);
+          }),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.pinkAccent[100],
+          onPressed: () {
+            final Future<Transferencia> future =
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return FormularioTransferencia();
+            }));
+            future.then((transferenciaRecebida) {
+              debugPrint('$transferenciaRecebida');
+              if (transferenciaRecebida != null) {
+                setState(() {
+                  widget._transferencias.add(transferenciaRecebida);
+                });
+              }
+            });
+          }),
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.valor.toString()),
+          subtitle: Text(_transferencia.numeroConta.toString()),
+        ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
+
+  Transferencia(this.valor, this.numeroConta);
+}
